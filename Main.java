ab8babd0e877a4efc2caec79c5a4fba6528f5753
Pedro Augusto <ptssar22@gmail.com>
2025-11-18 22:47:49 -0300

    fix: all problems

--- Main.java
+++ Main.java
@@ -14,6 +14,8 @@ public class Main {
 
     static LinkedList list = new LinkedList();
 
+    static final int NULL = -999999999;
+
     private static void loadToMemory() {
         short counter = 0;
 
@@ -25,7 +27,7 @@ public class Main {
 
                 int lineNumber = -1;
 
-                Pattern _pattern = Pattern.compile("^([0-9]+) ([A-Za-z]+ [A-Za-z]+ ?[A-Za-z0-9]*)");
+                Pattern _pattern = Pattern.compile("^([0-9]+) ([A-Za-z0-9 -]*)");
                 Matcher _matcher = _pattern.matcher(data);
 
                 if(_matcher.find()) {
@@ -49,6 +51,10 @@ public class Main {
         char[] registers    = "ABCDEFGHIJKLMNOPQRSWXYZ".toCharArray();;
         int[] values        = new int[26];
 
+        for(int i = 0; i < values.length; i++) {
+            values[i] = -999999999;
+        }
+
         short registerCounter = 0; 
         short errorCode = 0;
 
@@ -68,6 +74,12 @@ public class Main {
 
             String instruction = code.substring(0, 3).toUpperCase();
 
+            short loopCode = 0;
+            if(Character.isDigit(firstRegister)) {
+                System.out.println("ERRO: o primeiro valor precisa ser um registrador!");
+                break;
+            }
+
             if(!instruction.equals("OUT") && !instruction.equals("INC") && !instruction.equals("DEC") && code.length() < 6) {
                 System.out.println("ERRO: Falta de segundo registrador na linha " + lineNumber + " do código");
                 break;
@@ -75,19 +87,28 @@ public class Main {
 
             switch (instruction) {
                 case "MOV":
+                    loopCode = 1;
                     for(int i = 0; i < registers.length; i++) {
                         if(firstRegister == registers[i]) {
                             String value = code.toUpperCase().substring(6);
                             // It is a numerical value
                             if(value.matches("^-?[0-9]*$")) {
                                 values[i] = Integer.parseInt(value); 
+                                loopCode = 0;
                                 break;
                             } 
                             // It is not a numerical value
                             else {
                                 for(int j = 0; j < registers.length; j++) {
                                     if(value.charAt(0) == registers[j]) {
-                                        values[i] = values[j]; 
+                                        if(values[j] != NULL) {
+                                            values[i] = values[j]; 
+                                            loopCode = 0;
+                                        }
+                                        else {
+                                            System.out.println("ERRO: o segundo registrador precisa ser inicializado!");
+                                            errorCode = 1;
+                                        }
                                         break;
                                     }
                                 }
@@ -98,43 +119,81 @@ public class Main {
                     break;
 
                 case "OUT":
+                    loopCode = 1;
                     for(int i = 0; i < registers.length; i++) {
                         if(firstRegister == registers[i]) {
-                            System.out.println("O registrador " + firstRegister + " tem o valor: " + values[i]);
+                            if(values[i] != NULL) {
+                                System.out.println("O registrador " + firstRegister + " tem o valor: " + values[i]);
+                                loopCode = 0;
+                            }
+                            else {
+                                System.out.println("ERRO: É necessário inicializar o registrador antes de utilizado!");
+                                errorCode = 1;
+                            }
                             break;
                         }
                     }
                     break;
 
                 case "INC":
+                    loopCode = 1;
                     for(int i = 0; i < registers.length; i++) {
                         if(firstRegister == registers[i]) {
-                            values[i] += 1;
+                            if(values[i] != NULL) {
+                                values[i] += 1;
+                                loopCode = 0;
+                            }
+                            else {
+                                System.out.println("ERRO: é necessário inicializar o registrador antes de utilizado!");
+                                errorCode = 1;
+                            }
                             break;
                         }
                     }
                     break;
 
                 case "DEC":
+                    loopCode = 1;
                     for(int i = 0; i < registers.length; i++) {
                         if(firstRegister == registers[i]) {
-                            values[i] -= 1;
-                            break;
+                            if(values[i] != NULL) {
+                                values[i] -= 1;
+                                loopCode = 0;
+                            }
+                            else {
+                                System.out.println("ERRO: é necessário inicializar o registrador antes de utilizado!");
+                                errorCode = 1;
+                            }
                         }
                     }
                     break;
 
                 case "ADD":
+                    loopCode = 1;
                     for(int i = 0; i < registers.length; i++) {
                         if(firstRegister == registers[i]) {
                             String value = code.toUpperCase().substring(6);
-                            if(value.matches("^-?[0-9]*$")) {
-                                values[i] += Integer.parseInt(value);
+                            if(value.matches("^-?[0-9]*$erro")) {
+                                if(values[i] != NULL) {
+                                    values[i] += Integer.parseInt(value);
+                                    loopCode = 0;
+                                }
+                                else {
+                                    System.out.println("erro: é necessário inicializar o registrador antes de utilizado!");
+                                    errorCode = 1;
+                                }
                             }
                             else if(value.matches("^[a-zA-z]*$")){
                                 for(int j = 0; j < registers.length; j++) {
                                     if(value.charAt(0) == registers[j]) {
-                                        values[i] += values[j];
+                                        if(values[i] != NULL) {
+                                            values[i] += values[j];
+                                            loopCode = 0;
+                                        }
+                                        else {
+                                            System.out.println("erro: é necessário inicializar o registrador antes de utilizado!");
+                                            errorCode = 1;
+                                        }
                                     }
                                 }
                             }
@@ -148,17 +207,31 @@ public class Main {
                     break;
 
                 case "SUB":
+                    loopCode = 1;
                     for(int i = 0; i < registers.length; i++) {
                         if(firstRegister == registers[i]) {
                             String value = code.toUpperCase().substring(6);
                             if(value.matches("^-?[0-9]*$")) {
-                                values[i] -= Integer.parseInt(value);
+                                if(values[i] != NULL) {
+                                    values[i] -= Integer.parseInt(value);
+                                    loopCode = 0;
+                                }
+                                else {
+                                    System.out.println("erro: é necessário inicializar o registrador antes de utilizado!");
+                                    errorCode = 1;
+                                }
                             }
                             else if(value.matches("^[a-zA-Z]+$")){
                                 for(int j = 0; j < registers.length; j++) {
                                     if(value.charAt(0) == registers[j]) {
-                                        values[i] -= values[j];
-                                        break;
+                                        if(values[i] != NULL) {
+                                            values[i] -= values[j];
+                                            loopCode = 0;
+                                        }
+                                        else {
+                                            System.out.println("erro: é necessário inicializar o registrador antes de utilizado!");
+                                            errorCode = 1;
+                                        }
                                     }
                                 }
                             } 
@@ -172,17 +245,31 @@ public class Main {
                     break;
 
                 case "MUL":
+                    loopCode = 1;
                     for(int i = 0; i < registers.length; i++) {
                         if(firstRegister == registers[i]) {
                             String value = code.toUpperCase().substring(6);
                             if(value.matches("^-?[0-9]*$")) {
-                                values[i] *= Integer.parseInt(value);
+                                if(values[i] != NULL) {
+                                    values[i] *= Integer.parseInt(value);
+                                    loopCode = 0;
+                                }
+                                else {
+                                    System.out.println("erro: é necessário inicializar o registrador antes de utilizado!");
+                                    errorCode = 1;
+                                }
                             }
                             else if(value.matches("^[a-zA-Z]+$")) {
                                 for(int j = 0; j < registers.length; j++) {
                                     if(value.charAt(0) == registers[j]) {
-                                        values[i] *= values[j];
-                                        break;
+                                        if(values[i] != NULL) {
+                                            values[i] *= values[j];
+                                            loopCode = 0;
+                                        }
+                                        else {
+                                            System.out.println("erro: é necessário inicializar o registrador antes de utilizado!");
+                                            errorCode = 1;
+                                        }
                                     }
                                 }
                                 break;
@@ -197,6 +284,7 @@ public class Main {
                     break;
 
                 case "DIV":
+                    loopCode = 1;
                     for(int i = 0; i < registers.length; i++) {
                         if(firstRegister == registers[i]) {
                             String value = code.toUpperCase().substring(6);
@@ -207,13 +295,33 @@ public class Main {
                                     errorCode = 1;
                                 }
                                 else {
-                                    values[i] /= Integer.parseInt(value);
+                                    if(values[i] != NULL) {
+                                        values[i] /= Integer.parseInt(value);
+                                        loopCode = 0;
+                                    }
+                                    else {
+                                        System.out.println("erro: é necessário inicializar o registrador antes de utilizado!");
+                                        errorCode = 1;
+                                    }
                                 }
                             }
                             else if(value.matches("^[a-zA-z]*$")){
                                 for(int j = 0; j < registers.length; j++) {
                                     if(value.charAt(0) == registers[j]) {
-                                        values[i] /= values[j];
+                                        if(values[j] != 0) {
+                                            if(values[i] != NULL) {
+                                                values[i] *= values[j];
+                                                loopCode = 0;
+                                            }
+                                            else {
+                                                System.out.println("erro: é necessário inicializar o registrador antes de utilizado!");
+                                                errorCode = 1;
+                                            }
+                                        }
+                                        else {
+                                            System.out.println("ERRO: Divisão por zero!");
+                                            errorCode = 1;
+                                        }
                                     }
                                 }
                             }
@@ -227,6 +335,7 @@ public class Main {
                     break;
 
                 case "JNZ":
+                    loopCode = 1;
                     for(int i = 0; i < registers.length; i++) {
                         if(firstRegister == registers[i] && values[i] != 0) {
                             String value = code.toUpperCase().substring(6);
@@ -239,9 +348,13 @@ public class Main {
                                 else {
                                     pointer = gotoNode;
                                     pointerCode = 1;
+                                    loopCode = 0;
                                 }
                             }
                             break;
+                        }
+                        else if(values[i] == 0) {
+                            loopCode = 0;
                         }
                     }
                     break;
@@ -253,6 +366,9 @@ public class Main {
             }
 
             if(errorCode != 0) break;
+            if(loopCode == 1) {
+                System.out.println("ERRO: Valor incorreto no lugar do registrador!");
+            }
             if(pointerCode == 0) {
                 pointer = pointer.getNext();
             }
